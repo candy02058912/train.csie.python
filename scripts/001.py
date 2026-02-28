7f16f452b57add93f8e63ec74a0b8e928ce46b23
Candy Tsai <candy02058912@gmail.com>
2026-02-28 22:17:29 +0800

    feat: add basic web application script.

--- scripts/001.py
+++ scripts/001.py
@@ -1,1 +1,16 @@
-print("大家好！！！！")
+# /// script
+# dependencies = [
+#   "flask"
+# ]
+# ///
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "我就是一個網頁囉！"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
