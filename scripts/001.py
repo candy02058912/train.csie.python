e0483d2198402dcc15d5981e0450839d85b5e346
Candy Tsai <candy02058912@gmail.com>
2026-03-10 14:47:05 +0800

    refactor: improve 001.py

--- scripts/001.py
+++ scripts/001.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "我就是一個網頁囉！"
+    return "歡迎來上 Candy 的 Python 課！"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
