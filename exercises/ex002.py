13a9582470368b75ae36e0486282b86a8ab93f9f
Candy Tsai <candy02058912@gmail.com>
2026-03-10 12:13:59 +0800

    refactor: improve description for ex002.py

--- exercises/ex002.py
+++ exercises/ex002.py
@@ -1,6 +1,6 @@
 """
 title: 練習題 002：修好 Bug！
-quest_html: 哎呀！Sam 受傷了，但是目前的程式碼好像有點問題。原本設定好的傷害值 <code>sword_damage</code> 似乎沒有正確扣除。請修改程式碼，讓最後印出的 <code>end_health</code> 能正確顯示 Sam 受傷後的剩餘血量。
+quest_html: 哎呀！山姆受傷了，但是目前的程式碼好像有點問題。原本設定好的傷害值 <code>sword_damage</code> 似乎沒有正確扣除。請修改程式碼，讓最後印出的 <code>end_health</code> 能正確顯示 Sam 受傷後的剩餘血量。
 """
 
 # ==== STARTER CODE ====
@@ -10,9 +10,9 @@ start_health = 100
 end_health = start_health
 
 # 請勿更動下方的程式碼
-print(f"Sam's health is: {start_health}")
-print(f"Sam takes {sword_damage} damage...")
-print(f"Sam's health is: {end_health}")
+print(f"山姆的血量是: {start_health}")
+print(f"山姆受到 {sword_damage} 點傷害...")
+print(f"山姆的血量是: {end_health}")
 
 # ==== TEST CODE ====
 import sys
@@ -23,6 +23,6 @@ def run_tests():
         if end_health == 90:
             return "SUCCESS"
         else:
-            return f"Sam 的剩餘血量應該要是 90，但你的程式計算結果為 {end_health}。"
+            return f"山姆的剩餘血量應該要是 90，但你的程式計算結果為 {end_health}。"
     except Exception as e:
         return str(e)
