0dfac7f7bd905e95f4f6549dd92572f4098850c0
Candy Tsai <candy02058912@gmail.com>
2026-03-13 21:55:18 +0800

    feat: implement solution for ex010 Python exercise

--- exercises/ex010.py
+++ exercises/ex010.py
@@ -1,7 +1,7 @@
 """
 title: 練習題 010：角色狀態報表
 score: 1
-quest_html: 實習生寫了一個角色資料系統，但他對型別的概念一團糟！<br>請修正變數的給值方式，確保型別符合預期：<br>1. <code>name</code> 為字串 <code>"Candy"</code>。<br>2. <code>level</code> 為整數 <code>25</code>。<br>3. <code>character_class</code> 為字串 <code>"Windrunner"</code>。<br>4. <code>magic_resistance</code> 為浮點數 <code>15.0</code>。<br>5. <code>account_active</code> 為布林值 <code>True</code>。
+quest_html: 實習生寫了一個角色資料系統，但他對型別的概念一團糟！<br>請修正變數的給值方式，確保型別符合預期：<br>1. <code>name</code> 為字串 <code>"Candy"</code>。<br>2. <code>level</code> 為整數 <code>25</code>。<br>3. <code>character_class</code> 為字串 <code>"魔術師"</code>。<br>4. <code>magic_resistance</code> 為浮點數 <code>15.0</code>。<br>5. <code>account_active</code> 為布林值 <code>True</code>。
 """
 
 # ==== STARTER CODE ====
